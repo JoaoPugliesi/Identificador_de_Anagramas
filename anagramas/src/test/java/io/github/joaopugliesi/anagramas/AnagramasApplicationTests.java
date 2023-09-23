cb1183820851745a42dd04bed14ae0aade910b86
Joao Pugliesi <joaopaulo.allex@gmail.com>
2023-09-23 17:47:41 -0300

    Subindo projeto

---
+++ anagramas/src/test/java/io/github/joaopugliesi/anagramas/AnagramasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaopugliesi.anagramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
